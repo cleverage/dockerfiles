d637bff1ff10d7d676ce1e06b60859ef3a5cdb3d
Jean-Yves Gastaud <jygastaud@clever-age.com>
2018-11-28 17:09:08 +0100

    Set remote_host to start by default

--- eav-manager/php7.2/devfiles/entrypoint.sh
+++ eav-manager/php7.2/devfiles/entrypoint.sh
@@ -33,6 +33,7 @@ if [ "${INIT_XDEBUG_ACTIVATED}" -eq "1" ]; then
 
     sed -i "s/xdebug\.remote_autostart\=.*/xdebug\.remote_autostart\=1/g" ~/etc/php7/00-xdebug.ini
     sed -i "s/xdebug\.remote_enable\=.*/xdebug\.remote_enable\=1/g" ~/etc/php7/00-xdebug.ini
+    sed -i "s/xdebug\.remote_connect_back\=.*/xdebug\.remote_connect_back\=1/g" ~/etc/php7/00-xdebug.ini
 
     if [ "${INIT_XDEBUG_REMOTE_PORT}" -eq "1" ]; then
         # Default to 9000
@@ -44,9 +45,7 @@ if [ "${INIT_XDEBUG_ACTIVATED}" -eq "1" ]; then
         sed -i "s/xdebug\.idekey\=.*/xdebug\.idekey\=${INIT_XDEBUG_IDEKEY}/g" ~/etc/php7/00-xdebug.ini
     fi
 
-    if [ -z "${INIT_XDEBUG_REMOTE_HOST}" ]; then
-        sed -i "s/xdebug\.remote_connect_back\=.*/xdebug\.remote_connect_back\=1/g" ~/etc/php7/00-xdebug.ini
-    else
+    if [ "${INIT_XDEBUG_REMOTE_HOST}" ]; then
         sed -i "s/#xdebug\.remote_host\=.*/xdebug\.remote_host\=${INIT_XDEBUG_REMOTE_HOST}/g" ~/etc/php7/00-xdebug.ini
     fi
 fi
