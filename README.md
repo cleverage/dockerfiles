50d4da0e1c57942a02568c0cf954a792dd13c8ca
Jean-Yves CAMIER <jycamier@clever-age.com>
2019-03-21 16:01:03 +0100

    fix xdebug for mac

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Clever Age's Dockerfiles
 | INIT_XDEBUG_ACTIVATED   | 0              | Active Xdebug  |
 | INIT_XDEBUG_REMOTE_PORT | 9000           | Xdebug conf    |
 | INIT_XDEBUG_IDEKEY      | PHPSTORM       | Xdebug IDEKey  |
+| INIT_XDEBUG_REMOTE_CONNECT_BACK      | 0       | Active (or not) remote connect back  |
 | INIT_XDEBUG_REMOTE_HOST | ~              | Xdebug Remote Host (if custom) |
 | INIT_COMPOSER_INSTALL   | ~              | Launch Composer install at entrypoint |
 | INIT_PHP_ENABLED_MODULES   | ~              | String of space-separated modules to enabled (for meminfo) |
--- eav-manager/php7.2/devfiles/entrypoint.sh
+++ eav-manager/php7.2/devfiles/entrypoint.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
+
 set -e
 
 export INIT_COMPOSER_INSTALL
 
 export INIT_XDEBUG_ACTIVATED
+export INIT_XDEBUG_REMOTE_CONNECT_BACK
 export INIT_XDEBUG_REMOTE_HOST
 export INIT_XDEBUG_REMOTE_PORT
 export INIT_XDEBUG_IDEKEY
 export INIT_PHP_ENABLED_MODULES
 
+echo "#########"
+echo "## Environment variables"
+echo "#########"
+
+env | grep INIT
+
+echo "#########"
+
 ### INIT PROJECT WITH COMPOSER INSTALL
 ### ---
 ### - generate parameters.yml with override by environment variable
 ### - generate cache
-if [ "${INIT_COMPOSER_INSTALL}" -eq "1" ]; then
+if [[ "${INIT_COMPOSER_INSTALL}" -eq "1" ]]; then
 
-    ## Docker-sync (MacOS tool to sync code asynchronously to increase performance)
-    ## Mandatory if you need to x
-    while [ ! -f composer.json ] ;
-    do
-        echo "Wait for composer.json file"
-        sleep 2
-    done
+    echo "-------"
+    echo "Composer Install"
+    echo "-------"
 
     composer install --no-ansi --no-interaction --no-progress --optimize-autoloader
 fi
@@ -30,23 +36,28 @@ fi
 ### ---
 ### - dynamic configuration
 ### - active xdebug before fpm started
-if [ "${INIT_XDEBUG_ACTIVATED}" -eq "1" ]; then
+if [[ "${INIT_XDEBUG_ACTIVATED}" -eq "1" ]]; then
+
+    echo "-------"
+    echo "Xdebug Activated"
+    echo "-------"
 
     sed -i "s/xdebug\.remote_autostart\=.*/xdebug\.remote_autostart\=1/g" ~/etc/php7/00-xdebug.ini
     sed -i "s/xdebug\.remote_enable\=.*/xdebug\.remote_enable\=1/g" ~/etc/php7/00-xdebug.ini
-    sed -i "s/xdebug\.remote_connect_back\=.*/xdebug\.remote_connect_back\=1/g" ~/etc/php7/00-xdebug.ini
+    sed -i "s/xdebug\.remote_connect_back\=.*/xdebug\.remote_connect_back\=${INIT_XDEBUG_REMOTE_CONNECT_BACK:-0}/g" ~/etc/php7/00-xdebug.ini
 
-    if [ "${INIT_XDEBUG_REMOTE_PORT}" -eq "1" ]; then
+    if [[ "${INIT_XDEBUG_REMOTE_PORT}" -eq "1" ]]; then
         # Default to 9000
         sed -i "s/xdebug\.remote_port\=.*/xdebug\.remote_port\=${INIT_XDEBUG_REMOTE_PORT}/g" ~/etc/php7/00-xdebug.ini
     fi
 
-    if [ -n "${INIT_XDEBUG_IDEKEY}" ]; then
+    if [[ -n ${INIT_XDEBUG_IDEKEY} ]]; then
         # Default to PHPSTORM
         sed -i "s/xdebug\.idekey\=.*/xdebug\.idekey\=${INIT_XDEBUG_IDEKEY}/g" ~/etc/php7/00-xdebug.ini
     fi
 
-    if [ -n "${INIT_XDEBUG_REMOTE_HOST}" ]; then
+    if [[ -n ${INIT_XDEBUG_REMOTE_HOST} ]]; then
+        # Default to host.docker.internal
         sed -i "s/#xdebug\.remote_host\=.*/xdebug\.remote_host\=${INIT_XDEBUG_REMOTE_HOST}/g" ~/etc/php7/00-xdebug.ini
     fi
 fi
